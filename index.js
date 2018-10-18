04132ed51c4583919c09bdab727980319bf3588d
Acer 2 <sava.dumitru.daniel@gmail.com>
2018-10-18 15:48:59 +0300

    moved uidGenerator from pskcrypto

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 module.exports.OwM = require("./lib/OwM");
 module.exports.Queue = require("./lib/Queue");
+const uidGenerator = require("./lib/uidGenerator").createUidGenerator(200, 32);
+
+module.exports.uidGenerator = uidGenerator;
+module.exports.generateUid = uidGenerator.generateUid;
