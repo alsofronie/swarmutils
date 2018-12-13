d15506951648e3a3fb4c78e9f5b78719c63989eb
asac <asac.cool@gmail.com>
2018-12-13 17:22:26 +0200

    moved beeshealer and safe-uuid

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 module.exports.OwM = require("./lib/OwM");
-module.exports.Queue = require("./lib/Queue");
+module.exports.beesHealer = require("./lib/beesHealer");
+
 const uidGenerator = require("./lib/uidGenerator").createUidGenerator(200, 32);
+
+module.exports.safe_uuid = require("./lib/safe-uuid").init(uidGenerator);
+
+module.exports.Queue = require("./lib/Queue");
+module.exports.combos = require("./lib/Combos");
 
 module.exports.uidGenerator = uidGenerator;
 module.exports.generateUid = uidGenerator.generateUid;
-module.exports.combos = require("./lib/Combos");
+
 module.exports.createPskConsole = function () {
   return require('./lib/pskconsole');
 };
+
+
+if(typeof global.$$ == "undefined"){
+  global.$$ = {};
+}
+
+if(typeof global.$$.uidGenerator == "undefined"){
+    $$.uidGenerator = module.exports.safe_uuid;
+}
